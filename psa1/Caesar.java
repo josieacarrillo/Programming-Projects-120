65a28875548ae613a34d8d439f8f140f2c5e34fc
Your Name <josieacarrillo@gmail.com>
2018-10-08 18:31:53 -0700

    first commit

---
+++ psa1/Caesar.java
@@ -0,0 +1,33 @@
+// Keep these two lines.  They are what tell Java to include the
+// classes you need for working with files.
+// You might get warnings about them at first.  That's OK, just
+// ignore the warnings.  They should go away as you complete your code.
+import java.io.*;
+import java.util.*;
+
+/** Your header comment goes here.
+ * Be sure to include your name, cse account name, and the date 
+ * */
+public class Caesar {
+
+ public static char letterOperation(char letter, int rotation) {
+  return '0';
+ }
+
+ public static String encrypt(String message, int rotation) {
+  return "";
+ }
+
+ public static String decrypt(String message, int rotation) {
+  return "";
+ }
+
+ public static String encryptTwo(String message, int rotation) {
+  return "";
+ }
+ 
+ public static String decryptTwo(String message, int rotation) {
+  return "";
+ }
+
+}
