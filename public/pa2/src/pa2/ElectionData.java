4d689621f314cac3410c1370b9cbbf259bb1a89d
Your Name <josieacarrillo@gmail.com>
2018-10-27 16:07:14 -0700

    finsihed menu page where it asks user what they would like to know

--- public/pa2/src/pa2/ElectionData.java
+++ public/pa2/src/pa2/ElectionData.java
@@ -28,6 +28,7 @@ public class ElectionData {
 		System.out.println("        "+ "4"+" 	"+"Which states switched party affiliation between 2012 and 2016");
 		System.out.println("        "+"5"+ "       "+ "How many licks does it it take to get to the center of a tootsie pop");
 		System.out.println("        " + "0"+ "       "+ "Quit");
+		
 	}
 	
 
@@ -35,11 +36,12 @@ public class ElectionData {
 		Scanner input = new Scanner(System.in);
 		String choice = "";
 		
-while(!choice.equals("1") && !choice.equals("2")&& !choice.equals("3")&& !choice.equals("4")&& !choice.equals("5")){
+while(!choice.equals("0")&& !choice.equals("1") && !choice.equals("2")&& !choice.equals("3")&& !choice.equals("4")&& !choice.equals("5")){
 	printMenu();
 	
 	choice = input.nextLine();
-}
+	}
+
  while(choice.equals("1")){
 	
 	 System.out.println("You have entered: " + choice);
@@ -61,10 +63,13 @@ while(!choice.equals("1") && !choice.equals("2")&& !choice.equals("3")&& !choice
 	 System.out.println("TROLOLOLOL");
 	 choice= input.nextLine();
  }
+ while ( choice.equals("0")){
+	 System.out.println("Goodbye");
+	 choice = input.nextLine();
+ }
 }
 	
 	public static void main(String[] args){
-		runLoop();
 		
 		
 	
